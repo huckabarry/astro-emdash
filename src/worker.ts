8614f8b3ae8dd7c83c8eea641731a685295fe7fc
huckabarry <bryan.robb@gmail.com>
2026-08-01 00:08:51 -0700

    Update Emdash to 0.31.1

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,1 @@
-import handler from "@astrojs/cloudflare/entrypoints/server";
-
-export { PluginBridge } from "@emdash-cms/cloudflare/sandbox";
-
-export default {
-	async fetch(request: Request, env: any, ctx: ExecutionContext) {
-		return handler.fetch(request, env, ctx);
-	},
-};
+export { default, PluginBridge } from "@emdash-cms/cloudflare/worker";
